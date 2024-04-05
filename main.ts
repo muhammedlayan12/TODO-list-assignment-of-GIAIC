4a1ba57addd5fa31bc73282eaa8b3f51afe43a4a
Muhammed Layan <153948369+muhammedlayan12@users.noreply.github.com>
2024-04-05 20:36:37 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+let todos  :string[] = []; 
+let condition = true;
+
+
+
+
+while(condition){
+let addTask = await inquirer.prompt(
+    [
+        {
+            name: "todo",
+            type: "input",
+           message: "What do you want to add in your todo?"       
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "do you want to add more todo",
+            default: "false",
+        }
+    ]
+);
+todos.push(addTask.todo);
+condition = addTask.addMore;
+console.log(todos);
+}
